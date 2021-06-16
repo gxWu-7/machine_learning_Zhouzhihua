09c76d0952c33b1733e45d673d0e932a44ddf550
gxWu-7 <wuguoxi_217@163.com>
2021-06-17 02:59:03 +0800

    generate decision tree with gini index, no pruning, pre-pruning, post-pruning.

--- code/chapter_4/3.py
+++ code/chapter_4/3.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 
 
-class ID3DecisionTree(object):
+class InformationEntropyDecisionTree(object):
     def __init__(self,
                  is_leaf,
                  sub_trees,
@@ -54,12 +54,12 @@ class ID3DecisionTree(object):
         # 获取数据集在属性为attribute时的取值(集合)
         attribute_values = set(df.loc[:, attribute])
         split_information_entropy = 0.0
-        cur_data_information_entropy = ID3DecisionTree.compute_information_entropy(df)
+        cur_data_information_entropy = InformationEntropyDecisionTree.compute_information_entropy(df)
 
         for attribute_value in attribute_values:
             sub_df_k = df[df.loc[:, attribute] == attribute_value]
             p_k = sub_df_k.shape[0] * 1.0 / df.shape[0]
-            split_information_entropy += p_k * ID3DecisionTree.compute_information_entropy(sub_df_k)
+            split_information_entropy += p_k * InformationEntropyDecisionTree.compute_information_entropy(sub_df_k)
 
         cur_entropy_gain = cur_data_information_entropy - split_information_entropy
         return cur_entropy_gain
@@ -79,7 +79,7 @@ class ID3DecisionTree(object):
         continuous_values.sort()
 
         # 选取最优的划分点
-        dataset_information_entropy = ID3DecisionTree.compute_information_entropy(df)
+        dataset_information_entropy = InformationEntropyDecisionTree.compute_information_entropy(df)
         best_split_value = 0
         max_entropy_gain = -np.inf
         for index in range(0, len(continuous_values) - 1):
@@ -88,8 +88,8 @@ class ID3DecisionTree(object):
             df_less = df[df.loc[:, attribute] < split_value]
             df_more_or_equal = df[df.loc[:, attribute] >= split_value]
 
-            entropy_less = ID3DecisionTree.compute_information_entropy(df_less)
-            entropy_more_or_equal = ID3DecisionTree.compute_information_entropy(df_more_or_equal)
+            entropy_less = InformationEntropyDecisionTree.compute_information_entropy(df_less)
+            entropy_more_or_equal = InformationEntropyDecisionTree.compute_information_entropy(df_more_or_equal)
 
             p_less = df_less.shape[0] * 1.0 / df.shape[0]
             p_more_or_equal = df_more_or_equal.shape[0] * 1.0 / df.shape[0]
@@ -112,9 +112,9 @@ class ID3DecisionTree(object):
         :return: ID3DecisionTree对象
         """
         if has_continuous_attribute:
-            return ID3DecisionTree.generate_decision_tree_with_continuous_attribute(df)
+            return InformationEntropyDecisionTree.generate_decision_tree_with_continuous_attribute(df)
         else:
-            return ID3DecisionTree.generate_decision_tree_without_continuous_attribute(df)
+            return InformationEntropyDecisionTree.generate_decision_tree_without_continuous_attribute(df)
 
     @staticmethod
     def generate_decision_tree_with_continuous_attribute(df: pd.DataFrame):
@@ -130,14 +130,14 @@ class ID3DecisionTree(object):
 
         # 如果数据样本全部属于同一个类别，该节点不用划分，设置为叶子节点后返回
         if len(labels) == 1:
-            return ID3DecisionTree(is_leaf=True,
-                                   sub_trees=df.iloc[0, 0])
+            return InformationEntropyDecisionTree(is_leaf=True,
+                                                  sub_trees=df.iloc[0, 0])
 
         # 如果属性为空集或者仅有一个属性且取值相同，返回样本数最多的类
         if len(attributes) == 0 or (len(attributes == 1) and len(set(df.iloc[:, 0])) == 1):
             max_count_label = pd.value_counts(df.iloc[:, -1]).index[0]
-            return ID3DecisionTree(is_leaf=True,
-                                   sub_trees=max_count_label)
+            return InformationEntropyDecisionTree(is_leaf=True,
+                                                  sub_trees=max_count_label)
 
         # 从attributes中选择最佳的划分属性
         best_split_attribute = None
@@ -149,7 +149,7 @@ class ID3DecisionTree(object):
             if df.loc[:, attribute].dtype == np.float or df.loc[:, attribute].dtype == np.int:
                 # 如果该属性取值为连续值
                 cur_entropy_gain, split_value \
-                    = ID3DecisionTree.get_continuous_entropy_gain_and_attribute_split_value(df, attribute)
+                    = InformationEntropyDecisionTree.get_continuous_entropy_gain_and_attribute_split_value(df, attribute)
 
                 if cur_entropy_gain > max_entropy_gain:
                     best_split_attribute = attribute
@@ -159,7 +159,7 @@ class ID3DecisionTree(object):
 
             else:
                 # 如果该属性取值为非连续值
-                cur_entropy_gain = ID3DecisionTree.get_discontinuous_entropy_gain(df, attribute)
+                cur_entropy_gain = InformationEntropyDecisionTree.get_discontinuous_entropy_gain(df, attribute)
 
                 if cur_entropy_gain > max_entropy_gain:
                     best_split_attribute = attribute
@@ -169,11 +169,11 @@ class ID3DecisionTree(object):
             print('{}: {}'.format(attribute, cur_entropy_gain))
 
         # 开始划分树节点
-        tree = ID3DecisionTree(is_leaf=False,
-                               attribute=best_split_attribute,
-                               is_continuous=attribute_is_continuous,
-                               continuous_split_value=best_split_value,
-                               sub_trees={})
+        tree = InformationEntropyDecisionTree(is_leaf=False,
+                                              attribute=best_split_attribute,
+                                              is_continuous=attribute_is_continuous,
+                                              continuous_split_value=best_split_value,
+                                              sub_trees={})
 
         if attribute_is_continuous:
             # 使用连续值属性划分子树
@@ -186,9 +186,9 @@ class ID3DecisionTree(object):
             df_more_or_equal.drop(best_split_attribute, axis=1, inplace=True)
 
             tree.sub_trees[split_less] \
-                = ID3DecisionTree.generate_decision_tree_with_continuous_attribute(df_less)
+                = InformationEntropyDecisionTree.generate_decision_tree_with_continuous_attribute(df_less)
             tree.sub_trees[split_more_or_equal] \
-                = ID3DecisionTree.generate_decision_tree_with_continuous_attribute(df_more_or_equal)
+                = InformationEntropyDecisionTree.generate_decision_tree_with_continuous_attribute(df_more_or_equal)
         else:
             # 使用离散值属性划分
             best_split_attribute_values = set(df.loc[:, best_split_attribute])
@@ -201,7 +201,7 @@ class ID3DecisionTree(object):
 
                 # 递归生成子树
                 tree.sub_trees[attribute_value] \
-                    = ID3DecisionTree.generate_decision_tree_without_continuous_attribute(sub_df_drop_attribute)
+                    = InformationEntropyDecisionTree.generate_decision_tree_without_continuous_attribute(sub_df_drop_attribute)
 
         return tree
 
@@ -219,20 +219,20 @@ class ID3DecisionTree(object):
 
         # 如果数据样本全部属于同一个类别，该节点不用划分，设置为叶子节点后返回
         if len(labels) == 1:
-            return ID3DecisionTree(is_leaf=True,
-                                   sub_trees=df.iloc[0, 0])
+            return InformationEntropyDecisionTree(is_leaf=True,
+                                                  sub_trees=df.iloc[0, 0])
 
         # 如果属性为空集或者仅有一个属性且取值相同，返回样本数最多的类
         if len(attributes) == 0 or (len(attributes == 1) and len(set(df.iloc[:, -1])) == 1):
             max_count_label = pd.value_counts(df.iloc[:, -1]).index[0]
-            return ID3DecisionTree(is_leaf=True,
-                                   sub_trees=max_count_label)
+            return InformationEntropyDecisionTree(is_leaf=True,
+                                                  sub_trees=max_count_label)
 
         # 从attributes中选择最佳的划分属性
         best_split_attribute = None
         max_entropy_gain = -np.inf
         for attribute in attributes:
-            cur_entropy_gain = ID3DecisionTree.get_discontinuous_entropy_gain(df, attribute)
+            cur_entropy_gain = InformationEntropyDecisionTree.get_discontinuous_entropy_gain(df, attribute)
 
             if cur_entropy_gain > max_entropy_gain:
                 best_split_attribute = attribute
@@ -240,9 +240,9 @@ class ID3DecisionTree(object):
             # print('{}: {}'.format(attribute, cur_entropy_gain))
 
         # 开始划分树节点
-        tree = ID3DecisionTree(is_leaf=False,
-                               attribute=best_split_attribute,
-                               sub_trees={})
+        tree = InformationEntropyDecisionTree(is_leaf=False,
+                                              attribute=best_split_attribute,
+                                              sub_trees={})
         best_split_attribute_values = set(df.loc[:, best_split_attribute])
         for attribute_value in best_split_attribute_values:
             # 挑选出原数据集中在属性best_split_attribute取值为attribute_value的子df
@@ -253,7 +253,7 @@ class ID3DecisionTree(object):
 
             # 递归生成子树
             tree.sub_trees[attribute_value] \
-                = ID3DecisionTree.generate_decision_tree_without_continuous_attribute(sub_df_drop_attribute)
+                = InformationEntropyDecisionTree.generate_decision_tree_without_continuous_attribute(sub_df_drop_attribute)
         return tree
 
 
@@ -262,10 +262,10 @@ if __name__ == '__main__':
 
     # 使用不含连续值属性的数据集生成决策树
     df_has_not_continuous_attributes = df.drop(['编号', '密度', '含糖率'], axis=1)
-    tree_has_not_continuous_attributes = ID3DecisionTree.generate_decision_tree(df=df_has_not_continuous_attributes,
-                                                                                has_continuous_attribute=False)
+    tree_has_not_continuous_attributes = InformationEntropyDecisionTree.generate_decision_tree(df=df_has_not_continuous_attributes,
+                                                                                               has_continuous_attribute=False)
 
     # 使用包含连续值属性的数据集生成决策树
     df_has_continue_attributes = df.drop(['编号'], axis=1)
-    tree_has_continuous_attributes = ID3DecisionTree.generate_decision_tree(df=df_has_continue_attributes)
+    tree_has_continuous_attributes = InformationEntropyDecisionTree.generate_decision_tree(df=df_has_continue_attributes)
 
